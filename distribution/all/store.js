e2c87f10d21838eb213149cf5d8ec4b16e8a06da
ireyesp107 <isaias_reyes-paredes@brown.edu>
2024-03-13 21:29:26 +0000

    Trying mem gradescope

--- distribution/all/store.js
+++ distribution/all/store.js
@@ -1,65 +1,97 @@
 const id = require('../util/id');
-//const utils = require('../util'); // Assuming this contains the hash functions and idToNum
-const local= require('../local/local');
-
-// Assuming a function to get all NIDs of the nodes in the group is available
-function getAllNIDs() {
-    // This function needs to return an array of all NIDs (Node IDs) in the distributed system
-    // It's a placeholder here and needs an actual implementation based on your system's architecture
-    return [];
-}
+const local = require('../local/local');
+const globalDistribution = require('../../distribution.js'); // Ensure this path is correct
 
 let context = {};
 
 let store = (config) => {
-    context.gid = config.gid || "all";
-    context.hash = config.hash || id.naiveHash;
+    context.gid = config.gid || "all"; // Node group ID
+    context.hash = config.hash || id.naiveHash; // Hash function
+    
 
     return {
-        put: (key, value, callback) => {
-            const kid = id.getID(key);
-            const nids = getAllNIDs();
-            const targetNID = context.hash(kid, nids);
-            // Assuming you have a way to resolve NID to a node's address
-            const targetNode = resolveNIDtoNode(targetNID);
+        put: (value, key, callback) => {
 
-            if (targetNode.nid === id.getNID(global.nodeConfig)) { // Check if the target node is the current node
-                // If yes, perform local operation
-                local.mem.put(key, value, callback);
+            let groupMap = {}
+            local.groups.get(context.gid,(e, v) => {
+            //global.distribution[context.gid].groups.get(context.gid , (e, v) => {
+            groupMap = v
+            console.log(groupMap)
+
+            let nids = []
+                    for (const node in groupMap) {
+                    if (Object.prototype.hasOwnProperty.call(v, node)) {
+
+                        nids.push(id.getNID(groupMap[node]))
+                        // groupMap[id.getNID(groupMap[node])] = groupMap[node]
+                        // delete groupMap[node];
+                    }}
+            let enhancedKey = {key: key, gid: context.gid}
+            const kid = id.getID(enhancedKey);
+             const targetNID = context.hash(kid, nids);
+             const targetNode = groupMap[targetNID.substring(0,5)];
+            
+            if (targetNode.nid === id.getNID(global.nodeConfig)) {
+                local.mem.put([value,enhancedKey],callback);
             } else {
-                // Otherwise, send the operation to the target node
-                local.comm.send({key: key, value: value, gid: context.gid}, {node: targetNode, service: 'mem', method: 'put'}, callback);
+                local.comm.send([value,enhancedKey], {node: targetNode, service: 'mem', method: 'put'},callback)
             }
+        })
         },
 
         get: (key, callback) => {
-            const kid = id.getID(key);
-            const nids = getAllNIDs();
-            const targetNID = context.hash(kid, nids);
-            const targetNode = resolveNIDtoNode(targetNID);
+            let groupMap = {}
+            local.groups.get(context.gid,(e, v) => {
+            //global.distribution[context.gid].groups.get(context.gid , (e, v) => {
+            groupMap = v
 
+            let nids = []
+                    for (const node in groupMap) {
+                    if (Object.prototype.hasOwnProperty.call(v, node)) {
+
+                        nids.push(id.getNID(groupMap[node]))
+                        // groupMap[id.getNID(groupMap[node])] = groupMap[node]
+                        // delete groupMap[node];
+                    }}
+            let enhancedKey = {key: key, gid: context.gid}
+            const kid = id.getID(enhancedKey);
+             const targetNID = context.hash(kid, nids);
+             const targetNode = groupMap[targetNID.substring(0,5)];
             if (targetNode.nid === id.getNID(global.nodeConfig)) {
-                local.mem.get(key, callback);
+                local.mem.put([value,enhancedKey],callback);
             } else {
-                local.comm.send({key: key, gid: context.gid}, {node: targetNode, service: 'mem', method: 'get'}, callback);
+                local.comm.send([enhancedKey], {node: targetNode, service: 'mem', method: 'get'},callback)
             }
+        })
         },
 
         del: (key, callback) => {
-            const kid = id.getID(key);
-            const nids = getAllNIDs();
-            const targetNID = context.hash(kid, nids);
-            const targetNode = resolveNIDtoNode(targetNID);
+            let groupMap = {}
+            local.groups.get(context.gid,(e, v) => {
+            //global.distribution[context.gid].groups.get(context.gid , (e, v) => {
+            groupMap = v
 
+            let nids = []
+                    for (const node in groupMap) {
+                    if (Object.prototype.hasOwnProperty.call(v, node)) {
+
+                        nids.push(id.getNID(groupMap[node]))
+                        // groupMap[id.getNID(groupMap[node])] = groupMap[node]
+                        // delete groupMap[node];
+                    }}
+            let enhancedKey = {key: key, gid: context.gid}
+            const kid = id.getID(enhancedKey);
+             const targetNID = context.hash(kid, nids);
+             const targetNode = groupMap[targetNID.substring(0,5)];
             if (targetNode.nid === id.getNID(global.nodeConfig)) {
-                local.mem.del(key, callback);
+                local.mem.put([value,enhancedKey],callback);
             } else {
-                local.comm.send({key: key, gid: context.gid}, {node: targetNode, service: 'mem', method: 'del'}, callback);
+                local.comm.send([enhancedKey], {node: targetNode, service: 'mem', method: 'get'},callback)
             }
+        })
         },
 
         reconf: () => {
-            // Implementation for reconfiguration
         }
     };
 };
