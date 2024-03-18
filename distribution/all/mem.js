669902be65abfbf82b825b25f135c4bc85a43057
ireyesp107 <isaias_reyes-paredes@brown.edu>
2024-03-18 02:22:13 +0000

    trying mem reconf

--- distribution/all/mem.js
+++ distribution/all/mem.js
@@ -98,7 +98,51 @@ let mem = (config) => {
       });
     },
 
-    reconf: () => {
+    reconf: (oldGroupMap, callback) => {
+      let groupMap = {};
+      local.groups.get(context.gid, (e, v) => {
+        if (e) {
+          callback(e);
+          return;
+        }
+        groupMap = v;
+        let nids = [];
+        for (const node in groupMap) {
+          if (Object.prototype.hasOwnProperty.call(v, node)) {
+            nids.push(id.getNID(groupMap[node]));
+          }
+        }
+        let enhancedKey = {key: null, gid: context.gid};
+
+        local.mem.get(enhancedKey, (err, keys) => {
+          if (err) {
+            callback(err);
+            return;
+          }
+          let oldNIDs= Object.values(oldGroupMap).map((n) => id.getNID(n));
+          keys.forEach((key) => {
+            const kid = id.getID(key);
+            const oldTargetNID = context.hash(kid, oldNIDs);
+            const newTargetNID = context.hash(kid, nids);
+            const replaceNode = oldGroupMap[oldTargetNID.substring(0, 5)];
+            let replacedKey = {key: key, gid: context.gid};
+            if (oldTargetNID !== newTargetNID) {
+              local.comm.send([replacedKey],
+                  {node: replaceNode, service: 'mem', method: 'get'},
+                  (e, v)=>{
+                    const replaceObject = v;
+                    local.comm.send([replacedKey],
+                        {node: replaceNode, service: 'mem', method: 'del'},
+                        (e, v)=>{
+                          local.comm.send([replaceObject, replacedKey],
+                              {node: replaceNode, service: 'mem',
+                                method: 'put'}, callback);
+                        });
+                  });
+            }
+          });
+        });
+      });
     },
   };
 };
